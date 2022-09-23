27b7e86e854b763f5c209ab19e4fff0aa1ae7def
oswi <oswaldo.adrian@live.com.mx>
2022-09-23 10:03:29 -0500

    reference operator

---
+++ src/main/java/dev/ososuna/functional/classes/NameUtils.java
@@ -0,0 +1,20 @@
+package dev.ososuna.functional.classes;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class NameUtils {
+
+  public static void main(String[] args) {
+    List<String> professors = getList(
+      "John", "Paul", "George", "Ringo"
+    );
+    Consumer<String> printer = text -> System.out.println(text);
+    professors.forEach(printer);
+    professors.forEach(System.out::println);
+  }
+  static <T> List<T> getList(T... elements) {
+    return Arrays.asList(elements);
+  }
+}
